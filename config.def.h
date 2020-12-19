ba5654144a6cdc1130ccec78ad568dd35683b9a1
Manu Febie <imanuelfebie@gmail.com>
2020-12-19 20:11:08 +0700

    changed the default terminal to kitty instead of alacritty

--- config.def.h
+++ config.def.h
@@ -64,7 +64,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray5, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *browser[]  = { "firefox", NULL };
 static const char *gimp[]     = { "gimp", NULL};
 /* static const char *scrot[]    = { "scrot", "-q", "100", NULL}; */
